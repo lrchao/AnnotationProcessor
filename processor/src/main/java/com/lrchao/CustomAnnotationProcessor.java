a4004a549a35edad9a0d036abf632034e317a255
liuranchao <kevinliu@qeeka.com>
2016-08-03 17:10:00 +0800

    nothing

--- processor/src/main/java/com/lrchao/CustomAnnotationProcessor.java
+++ processor/src/main/java/com/lrchao/CustomAnnotationProcessor.java
@@ -5,23 +5,65 @@ import java.io.Writer;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Completion;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.JavaFileObject;
 
 @SupportedAnnotationTypes("com.lrchao.CustomAnnotation")
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
-public class CustomAnnotationProcessor extends AbstractProcessor{
+public class CustomAnnotationProcessor extends AbstractProcessor {
+
+    /**
+     * 主要的逻辑处理都在这里，这个方法里面可以实现扫描，处理注解，生成 java 文件。使用RoundEnvironment 参数，可以查询被特定注解标注的元素。
+     */
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
         System.out.println("annotations= " + annotations.size());
 
         System.out.println("roundEnv= " + roundEnv.getRootElements().size());
+
+        generateDialog(roundEnv);
+
+
+        return true;
+    }
+
+    /**
+     * 在这个方法里面你必须指定哪些注解应该被注解处理器注册。注意，它的返回值是一个String集合，包含了你的注解处理器想要处理的注解类型的全称
+     */
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return super.getSupportedAnnotationTypes();
+    }
+
+    /**
+     * 用来指定使用的java版本。通常返回SourceVersion.latestSupported()。
+     */
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return super.getSupportedSourceVersion();
+    }
+
+    @Override
+    public Iterable<? extends Completion> getCompletions(Element element, AnnotationMirror annotation, ExecutableElement member, String userText) {
+        return super.getCompletions(element, annotation, member, userText);
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+    }
+
+    private void generateDialog(RoundEnvironment roundEnv) {
         StringBuilder builder = new StringBuilder()
                 .append("package com.lrchao.annotationprocessor.generated;\n\n")
                 .append("public class GeneratedClass {\n\n") // open class
@@ -32,24 +74,17 @@ public class CustomAnnotationProcessor extends AbstractProcessor{
         // for each javax.lang.model.element.Element annotated with the CustomAnnotation
         for (Element element : roundEnv.getElementsAnnotatedWith(CustomAnnotation.class)) {
             String objectType = element.getSimpleName().toString();
-
-
-
             // this is appending to the return statement
             builder.append(objectType).append(" says hello1111!\\n");
         }
-
 
         builder.append("\";\n") // end return
                 .append("\t}\n") // close method
                 .append("}\n"); // close class
 
 
-
         try { // write the file
             JavaFileObject source = processingEnv.getFiler().createSourceFile("com.lrchao.annotationprocessor.generated.GeneratedClass");
-
-
             Writer writer = source.openWriter();
             writer.write(builder.toString());
             writer.flush();
@@ -58,8 +93,5 @@ public class CustomAnnotationProcessor extends AbstractProcessor{
             // Note: calling e.printStackTrace() will print IO errors
             // that occur from the file already existing after its first run, this is normal
         }
-
-
-        return true;
     }
 }
